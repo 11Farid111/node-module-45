88517c8dbdc805f668173ae844357119dc18d1c3
FARIDUL <fazila6940@gmail.com>
2024-05-20 14:36:27 +0600

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+
+
+const Contact = () => {
+      return (
+            <div>
+                  <h2>Contact Us</h2>
+                  <p>Please Call me</p>
+            </div>
+      );
+};
+
+export default Contact;
